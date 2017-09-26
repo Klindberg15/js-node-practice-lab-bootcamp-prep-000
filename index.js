9cbd613a5759e28b0d54c5ea69c2a3e912681c9a
Kari Lindberg <karisoolindberg@gmail.com>
2017-09-26 02:51:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 
 var name = "Kari"
 var height = "67"
+var message = '${name} is ${height} inches tall'
 
-console.log("Name:")
-console.log(name)
+module.exports = {name, height, message}
 
-console.log("Height:")
-console.log(parseInt(height)+1)
+console.log(message)
